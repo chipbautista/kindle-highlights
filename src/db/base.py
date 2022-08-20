8526e8966c2246d9e3457bcfd5a6395ee4b43915
chipbautista <chippybautista@gmail.com>
2022-08-20 21:50:39 +0800

    Add notes column to highlights table; fix db import errors

--- src/db/base.py
+++ src/db/base.py
@@ -1,4 +1,4 @@
-from db.base_class import Base
+from src.db.base_class import Base
 from src.models.author import Author
 from src.models.book import Book
 from src.models.highlight import Highlight
--- src/db/session.py
+++ src/db/session.py
@@ -1,4 +1,9 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
-# move db value to .env
-engine = create_engine("sqlite:///highlights.db", echo=True, future=True)
+
+def get_db() -> Session:
+    # move db value to .env
+    engine = create_engine("sqlite:///highlights.db", echo=True, future=True)
+    session = Session(engine)
+    return session
