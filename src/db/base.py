a064c4f9357e540a4c5e066a43534b8fb396e571
chipbautista <chippybautista@gmail.com>
2022-08-26 23:02:22 +0800

    Add files reqd for prev commit

--- src/db/base.py
+++ src/db/base.py
@@ -2,3 +2,4 @@ from src.db.base_class import Base
 from src.models.author import Author
 from src.models.book import Book
 from src.models.highlight import Highlight
+from src.models.metadata import Metadata
---
+++ src/settings.py
@@ -0,0 +1,6 @@
+BOOK_SEARCH_API_URL = "https://www.googleapis.com/books/v1/volumes?{params}"
+BOOK_SEARCH_DEFAULT_PARAMS = {
+    "langRestrict": "en",
+    "printType": "books",
+    "maxResults": 3,
+}
