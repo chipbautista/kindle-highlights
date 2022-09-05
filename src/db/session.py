03ed71b70d076fb55f3352e4d50e27c3c3cf73e6
chipbautista <chippybautista@gmail.com>
2022-09-05 21:08:28 +0800

    fix keyerror when .env is missing

--- src/db/session.py
+++ src/db/session.py
@@ -4,9 +4,10 @@ from sqlalchemy.orm import Session
 from dotenv import dotenv_values
 
 env = dotenv_values()
+local_sqlite_db = env.get("SQLITE_DB")
 
 
-def get_db(db_url: str = env["SQLITE_DB"]) -> Session:
+def get_db(db_url: str = local_sqlite_db) -> Session:
     # move db value to .env
     engine = create_engine(db_url, echo=True, future=True)
     session = Session(engine)
