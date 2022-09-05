d4d94cbe1decff69e4e01ba897d83df24202f3ce
chipbautista <chippybautista@gmail.com>
2022-09-05 20:56:01 +0800

    Get DB info from .env or st secrets

--- app/db_utils.py
+++ app/db_utils.py
@@ -3,9 +3,38 @@ import streamlit as st
 from src.db.base import Book
 from src.db.session import get_db
 
+import boto3
+from dotenv import dotenv_values
+
+env = dotenv_values()
+ENV = st.secrets.get("ENV", "dev")
+
+
+@st.cache
+def get_db_from_s3(aws_secrets: dict):
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id=aws_secrets["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key=aws_secrets["AWS_SECRET_ACCESS_KEY"],
+    )
+    s3.download_file(
+        aws_secrets["BUCKET"], aws_secrets["DB_FILE"], aws_secrets["DB_FILE"]
+    )
+    print("Database downloaded from S3.")
+    return aws_secrets["DB_FILE"]
+
+
+def get_app_db():
+    if ENV == "dev":
+        db = get_db()
+    else:
+        db_file = get_db_from_s3(st.secrets["aws"])
+        db = get_db(db_url=f"sqlite:///{db_file}")
+    return db
+
 
 # @st.cache -- cached outputs will be detached objects from DB
 def get_books():
-    db = get_db()
+    db = get_app_db()
     books = db.query(Book).all()
     return books
--- src/db/session.py
+++ src/db/session.py
@@ -1,9 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
+from dotenv import dotenv_values
 
-def get_db() -> Session:
+env = dotenv_values()
+
+
+def get_db(db_url: str = env["SQLITE_DB"]) -> Session:
     # move db value to .env
-    engine = create_engine("sqlite:///highlights.db", echo=True, future=True)
+    engine = create_engine(db_url, echo=True, future=True)
     session = Session(engine)
     return session
