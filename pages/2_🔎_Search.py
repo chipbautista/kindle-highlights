0a35fe4590cac2355604bd5f6e00ab459c2a07f1
chipbautista <chippybautista@gmail.com>
2022-09-07 21:50:49 +0800

    Add book selector for search results; improve highlighting

--- pages/2_🔎_Search.py
+++ pages/2_🔎_Search.py
@@ -1,4 +1,8 @@
+from typing import List, Set
+from re import sub, IGNORECASE
+
 import streamlit as st
+import pandas as pd
 
 from src.models.highlight import Highlight
 from pages.utils.db import search_highlights
@@ -7,17 +11,50 @@ from pages.utils.ui import show_highlight
 st.write("## Highlight Search")
 
 
-def highlight_search_query(text: str):
-    return text.replace(
-        query, f"<mark style='background-color: yellow;'>{query}</mark>"
+def highlight_search_query(text: str) -> str:
+    return sub(
+        rf"({query})",
+        "<mark style='background-color: yellow;'>\\1</mark>",
+        text,
+        flags=IGNORECASE,
+    )
+
+
+def format_book_num_results(book: str, book_result_count: pd.Series) -> str:
+    num_results = book_result_count[book]
+    return f"{book} ({num_results})"
+
+
+def show_book_selector(results: List[Highlight]) -> Set[str]:
+    books = pd.Series([r.book.title for r in results])
+    book_result_count = books.value_counts()
+    selected_books = st.multiselect(
+        "Select Books",
+        book_result_count.index,
+        format_func=lambda x: format_book_num_results(x, book_result_count),
+    )
+    return set(selected_books)
+
+
+def filter_results_by_book(
+    results: List[Highlight], selected_books: Set[str]
+) -> List[Highlight]:
+    return (
+        [r for r in results if r.book.title in selected_books]
+        if selected_books
+        else results
     )
 
 
 def show_results(results):
     if results:
         st.success(f"{len(results)} highlights found.")
+
+        selected_books = show_book_selector(results)
+        selected_results = filter_results_by_book(results, selected_books)
+
         st.markdown("---")
-        for i, result in enumerate(results):
+        for i, result in enumerate(selected_results):
             result.text = highlight_search_query(result.text)
             show_highlight(result, i, show_book_title=True)
 
